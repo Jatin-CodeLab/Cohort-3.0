abe02e03ecd5767ab24064a057745c383c9b234c
Jatin Prajapati <jatin.prajapati.frontenddev@gmail.com>
2026-09-15 13:57:24 +0530

    node.js in how to req and res

--- backend-Dev/expressJS/server.js
+++ backend-Dev/expressJS/server.js
@@ -1,3 +1,27 @@
-console.log("server script is running...");
+console.log("server.js file is running...");
 //?--------------------------------------------
 //?--------------------------------------------
+
+let http = require('http')
+
+let server = http.createServer((req, res) => {
+    // if (req.url === '/users') {
+    //     res.end("ye users API hai")
+    // }
+    // if (req.url === '/carsIMG') {
+    //     res.end("ye carsIMG API hai")
+    // }
+    // if (req.url === '/product') {
+    //     res.end("ye product API hai")
+    // }
+    // if (req.url === '/home') {
+    //     res.end("ye home API hai")
+    // }
+    //! this way not right in larger scale project
+
+})
+
+server.listen(5000, () => {
+    console.log('server start ho gaya hai');
+    
+})
